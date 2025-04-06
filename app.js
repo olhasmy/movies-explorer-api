6fda6d93fca04a33b190556cdbc9435977b3ae3a
olha <g.o.n.z.o.m.e@yandex.ru>
2025-04-06 22:19:34 +0300

    mongo

--- app.js
+++ app.js
@@ -32,15 +32,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Failed to connect to MongoDB:', err);
+mongoose.connect('mongodb://localhost:27017/moviesdb',
+  async (err) => {
+    if (err) throw err;
+    console.log('Conncted to moviesdb');
   });
 
 app.use(requestLogger);
